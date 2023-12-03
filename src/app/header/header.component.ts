d758c0cd0141f94856de40d614eee1f8c1e75042
Erlfred Joaquin <127403379+fredwakin@users.noreply.github.com>
2023-12-04 03:15:43 +0800

    Add files via upload

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { PostService } from '../post.service';
+import { BackEndService } from '../back-end.service';
+import { DarkModeService } from '../dark-mode.service';
+import { Post } from '../post.model';
+import { ViewChild, ElementRef } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit{
+
+  constructor(private backEndService: BackEndService, 
+              private postService: PostService, 
+              private darkModeService: DarkModeService, 
+              private router: Router){}
+
+  ngOnInit(): void {
+
+  }
+  onHomeClick() {
+    this.backEndService.fetchData().subscribe();
+  }
+  onSave(){
+    this.backEndService.saveData();
+  }
+
+  onFetch(){
+    this.backEndService.fetchData();
+  }
+}
---
+++ src/app/post-edit/post-edit.component.ts
@@ -0,0 +1,68 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Post } from '../post.model';
+import { PostService } from '../post.service';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { BackEndService } from '../back-end.service';
+
+@Component({
+  selector: 'app-post-edit',
+  templateUrl: './post-edit.component.html',
+  styleUrls: ['./post-edit.component.css']
+})
+export class PostEditComponent {
+
+  index: number=0;
+  form!: FormGroup;
+  editMode= false;
+    constructor( private postService: PostService, 
+                 private router: Router, 
+                 private actRoute: ActivatedRoute,
+                 private backEndService: BackEndService){}
+
+  ngOnInit(): void {
+    let title = '';
+    let image = '';
+    let description = '';
+    this.actRoute.params.subscribe((params: Params) => {
+      if(params['index']){
+        console.log(params['index']);
+this.index = params['index'];
+        const postSpec = this.postService.getSpecPost(this.index);
+          title = postSpec.title;
+          image = postSpec.image;
+          description = postSpec.description;
+          this.editMode=true;
+      }
+    })
+
+    this.form  = new FormGroup({
+      title: new FormControl (title, [Validators.required]),
+      image: new FormControl (image, [Validators.required]),
+      description: new FormControl (description, [Validators.required])
+    })
+  }
+
+  onSubmit(){
+    const title = this.form.value.title;
+    const image = this.form.value.image;
+    const description = this.form.value.description;
+
+
+    const post: Post = new Post(
+      title, image, description, 'Erlfred', new Date(), 0
+    );
+
+    if(this.editMode==false){
+      this.postService.addPost(post);
+    }
+    else{
+      this.postService.updatePost(this.index, post);
+    }
+    this.backEndService.saveData();
+    this.router.navigate(['/post-list'])
+  }
+  onCancel() {
+    this.router.navigate(['/post-list']);
+  }
+}
